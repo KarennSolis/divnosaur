1a7d727f29166c053d4da399565b76a9c66141ec
KarennSolis <127529504+KarennSolis@users.noreply.github.com>
2023-03-30 22:44:06 +0200

    Todo un poco
    Renombre de paginas, eliminación de formularios/carpeta, JS Index, JS Registro, Readme

---
+++ index.js
@@ -0,0 +1,31 @@
+password = document.getElementById('exampleInputPassword1');
+email = document.getElementById('exampleInputEmail1');
+form = document.getElementById('form');
+parrafo =document.getElementById('warnings');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let warnings = "";
+    let expRegEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let entrar = false;
+    parrafo.innerHTML = '';
+    if (!expRegEmail.test(email.value)) {
+        warnings += `El correo no es valido <br>`
+        entrar = true
+    };
+    if (password.value.length < 8) {
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    };
+    if (entrar) {
+        parrafo.innerHTML = warnings
+    } else {
+        setTimeout(function () {
+            location.href = 'http://127.0.0.1:5500/team-8/inicio_2.html';
+        }, 1000,);
+    };
+})
+
+home = document.getElementById('homeRegBot');
+boton = document.getElementById('regBot');
+
