83c5b80f61136e20b4788f41d133e90b608a8d27
KarennSolis <127529504+KarennSolis@users.noreply.github.com>
2023-05-11 19:10:47 +0200

    query

--- routes/followBack.js
+++ routes/followBack.js
@@ -6,7 +6,7 @@ const sequelize = require('../conexion-base-datos')
 const followers = async function(req, res) {
 	const user_id = req.params.user_id
 	try {
-		const followers = await sequelize.query(`SELECT DISTINCT * from users INNER JOIN friendship ON friendship.user_friend1_id  = users.user_id WHERE friendship.user_friend1_id = "${user_id}" AND friendship.status_friendship = 1`, {type: sequelize.QueryTypes.SELECT});
+		const followers = await sequelize.query(`SELECT DISTINCT * from users INNER JOIN friendship ON friendship.user_friend2_id  = users.user_id WHERE friendship.user_friend1_id = "${user_id}" AND friendship.status_friendship = 1`, {type: sequelize.QueryTypes.SELECT});
 
 	//   console.log(personas);
 	  res.send(followers);
