2ac5e43d81780e4b58b1063c341abcbfcb0c9ada
CrisSugar <51267439+CrisSugar@users.noreply.github.com>
2023-05-29 22:27:19 +0200

    commit icons

--- divnosaur/src/index.js
+++ divnosaur/src/index.js
@@ -6,7 +6,7 @@ import { AuthProvider } from './Context/AuthContext';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+/* import "bootstrap-icons/font/bootstrap-icons.css"; */
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap/dist/js/bootstrap";
